850d8842eb0d8e5965f1689df4118b5e7796e9f2
Denuwan Nammuni <denuwannammuni@gmail.com>
2025-01-29 19:33:49 +0530

    updates

---
+++ backend/routes/foodRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { addFood } from '../controllers/foodController.js';
+import multer from 'multer';
+
+const foodRouter = express.Router();
+
+foodRouter.post();
+
+
+
+
+export default foodRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoutes.js';
 
 //app config
 const app = express();
@@ -9,10 +11,18 @@ const port = 4000
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req,res) => {
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter)
+
+app.get("/", (req, res) => {
     res.send("API Working")
-})
+});
 
 app.listen(port, () => {
-    console.log(`Server Started on http://localhost: ${port}`)
-})
+    console.log(`Server Started on http://localhost:${port}`)
+});
+
+//mongodb+srv://BurgerQueen:<db_password>@cluster0.rc7zf.mongodb.net/?
